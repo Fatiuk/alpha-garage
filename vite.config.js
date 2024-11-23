317645d935f1803ef8e99426ed549d28468f8d9f
Fatiuk <125760945+Fatiuk@users.noreply.github.com>
2024-11-23 19:49:07 +0200

    FEAT: Init project

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
+import path from 'path';
 
 export default defineConfig({
   root: 'src',
@@ -12,4 +13,16 @@ export default defineConfig({
     outDir: '../dist',
   },
   plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+  css: {
+    preprocessorOptions: {
+      sass: {
+        additionalData: `@use "@/sass/vars.scss" as *;`,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 });
